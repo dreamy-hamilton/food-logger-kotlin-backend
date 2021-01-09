19c4ec6ecf45b817af3871feada9cbe32bd8fbd4
Martin Krombholz <martin.krombholz@example.com>
2021-01-09 12:56:28 +0100

    fix evn names

--- src/main/kotlin/me/dreamy_hamilton/food_logger_kotlin_backend/config/DataSourceConfig.kt
+++ src/main/kotlin/me/dreamy_hamilton/food_logger_kotlin_backend/config/DataSourceConfig.kt
@@ -9,13 +9,10 @@ import org.springframework.context.annotation.Configuration
 class DataSourceConfig {
     @Bean
     fun dataSourceAfterFlyway(dataSource: DataSource): DataSourceHolder {
-        println(System.getenv("JDBC_DATABASE_URL") + "\n username: " +
-            System.getenv("JDBC_DATABASE_USERNAME") + "\n password: " +
-            System.getenv("JDBC_DATABASE_PASSWORD"))
         val flyway = Flyway.configure().dataSource(
-            System.getenv("SPRING_DATABASE_URL"),
-            System.getenv("SPRING_DATABASE_USERNAME"),
-            System.getenv("SPRING_DATABASE_PASSWORD")
+            System.getenv("SPRING_DATASOURCE_URL"),
+            System.getenv("SPRING_DATASOURCE_USERNAME"),
+            System.getenv("SPRING_DATASOURCE_PASSWORD")
         ).load()
         flyway.migrate()
         return DataSourceHolder(dataSource)
