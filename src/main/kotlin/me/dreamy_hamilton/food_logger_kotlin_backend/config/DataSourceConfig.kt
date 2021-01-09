88d5e6043e8c0b44521ef5a7550b1cd18a515221
Martin Krombholz <martin.krombholz@example.com>
2021-01-09 15:40:09 +0100

    fix test

--- src/main/kotlin/me/dreamy_hamilton/food_logger_kotlin_backend/config/DataSourceConfig.kt
+++ src/main/kotlin/me/dreamy_hamilton/food_logger_kotlin_backend/config/DataSourceConfig.kt
@@ -2,19 +2,29 @@ package me.dreamy_hamilton.food_logger_kotlin_backend.config
 
 import javax.sql.DataSource
 import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
-class DataSourceConfig {
+class DataSourceConfig(
+    private val environment: Environment
+) {
+    @Value("\${spring.datasource.url}")
+    private lateinit var dataSourceUrl: String
+
+    @Value("\${spring.datasource.username}")
+    private lateinit var dataSourceUsername: String
+
+    @Value("\${spring.datasource.password}")
+    private lateinit var dataSourcePassword: String
+
     @Bean
     fun dataSourceAfterFlyway(dataSource: DataSource): DataSourceHolder {
-        println(System.getenv("SPRING_DATASOURCE_URL"))
-        val flyway = Flyway.configure().dataSource(
-            System.getenv("SPRING_DATASOURCE_URL"),
-            System.getenv("SPRING_DATASOURCE_USERNAME"),
-            System.getenv("SPRING_DATASOURCE_PASSWORD")
-        ).load()
+        val flyway = Flyway.configure()
+            .dataSource(dataSourceUrl, dataSourceUsername, dataSourcePassword)
+            .load()
         flyway.migrate()
         return DataSourceHolder(dataSource)
     }
